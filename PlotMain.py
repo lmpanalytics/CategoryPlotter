6881914277c9dd1ec9eed6d12eecfd0f3b567065
lmpanalytics2 <lmpconsulting@hotmail.com>
2018-08-21 14:39:44 +0200

    Apply a rolling 12 window function, looping through clusters and markets

--- PlotMain.py
+++ PlotMain.py
@@ -47,7 +47,7 @@ def fixMissingDates(df_grouped):
 
 
 def groupSalesByDates():
-    """ Group sales by dates in the data frame
+    """ Group sales by dates and return a R12 data frame
     """
     # Group by dates and sum sales
     df_grouped = df_sales.groupby('MyDate').sum()
@@ -55,11 +55,14 @@ def groupSalesByDates():
     # Handle missing dates
     df_grouped = fixMissingDates(df_grouped)
 
-    return(df_grouped)
+    # Apply a rolling 12 window
+    df_R12 = df_grouped.rolling(12).sum()
+
+    return(df_R12)
 
 
 def groupSalesByCluster(marketCluster):
-    """ Group sales by a cluster in the data frame
+    """ Group sales by a cluster and return a R12 data frame
     """
     # Filter on a market cluster
     df = df_sales.loc[df_sales['Cluster'] == marketCluster]
@@ -70,11 +73,17 @@ def groupSalesByCluster(marketCluster):
     # Handle missing dates
     df_grouped = fixMissingDates(df_grouped)
 
-    return(df_grouped)
+    # Drop 'Cluster' column to facilitate R12 window calculation
+    df_grouped = df_grouped.drop(['Cluster'], axis=1)
+
+    # Apply a rolling 12 window
+    df_R12 = df_grouped.rolling(12).sum()
+
+    return(df_R12)
 
 
 def groupSalesByMarket(market):
-    """ Group sales by a market in the data frame
+    """ Group sales by a market and return a R12 data frame
     """
     # Filter on a market
     df = df_sales.loc[df_sales['Market'] == market]
@@ -85,13 +94,37 @@ def groupSalesByMarket(market):
     # Handle missing dates
     df_grouped = fixMissingDates(df_grouped)
 
-    return(df_grouped)
+    # Drop 'Market' column to facilitate R12 window calculation
+    df_grouped = df_grouped.drop(['Market'], axis=1)
+
+    # Apply a rolling 12 window
+    df_R12 = df_grouped.rolling(12).sum()
+
+    return(df_R12)
 
 
-# print(df_sales)
+def extractGeographicalAreas(ColName):
+    """ Make set of Clusters or Markets
+    """
+    return set(df_sales[ColName])
+
+
+# Extract set of Clusters and Markets
+setOfClusters = extractGeographicalAreas('Cluster')
+setOfMarkets = extractGeographicalAreas('Market')
+
 print(groupSalesByDates())
-# print(groupSalesByCluster('Cluster1'))
-# print(groupSalesByMarket('Mkt1'))
+
+# Loop through Clusters and Markets
+for cluster in setOfClusters:
+    print("\n", cluster, ":")
+    print(groupSalesByCluster(cluster))
+
+
+for market in setOfMarkets:
+    print("\n", market, ":")
+    print(groupSalesByMarket(market))
+
 
 end = time.time()
 print('Execution time (sec): ', (end - start))
